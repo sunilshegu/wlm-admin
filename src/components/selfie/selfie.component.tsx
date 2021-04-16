7339577d1ac5f673036fe447832e138829130ee4
sunilshegu <sunilshegu@gmail.com>
2021-04-16 23:40:58 +0530

    Adds Css for all pop-ups

--- src/components/selfie/selfie.component.tsx
+++ src/components/selfie/selfie.component.tsx
@@ -52,7 +52,10 @@ class Selfie extends React.Component<DispatchProps & StateProps> {
                         <tr>
                             <th scope="col w-100">First</th>
                             <th scope="col w-100">Last</th>
-                            <th scope="col">Check</th>
+                            <th scope="col">About</th>
+                            <th scope="col">Selfie</th>
+                            <th scope="col">Images</th>
+                            <th scope="col">Id</th>
                         </tr>
                     </thead>
 
@@ -63,7 +66,16 @@ class Selfie extends React.Component<DispatchProps & StateProps> {
                                     <td>{user.firstName}</td>
                                     <td>{user.lastName}</td>
                                     <td>
-                                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onClick={() => this.selectUser(user._id)}>Check</button>
+                                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#aboutModal" onClick={() => this.selectUser(user._id)}>About</button>
+                                    </td>
+                                    <td>
+                                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#selfieModal" onClick={() => this.selectUser(user._id)}>Selfie</button>
+                                    </td>
+                                    <td>
+                                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#photosModal" onClick={() => this.selectUser(user._id)}>Images</button>
+                                    </td>
+                                    <td>
+                                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#idModal" onClick={() => this.selectUser(user._id)}>Id</button>
                                     </td>
                                 </tr>
                             ))
@@ -85,33 +97,197 @@ class Selfie extends React.Component<DispatchProps & StateProps> {
                         activeClassName={'active'}
                     />
                 </div>
-
-                {/* model html */}
-                <div className="modal fade" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                {/* About model html */}
+                <div className="modal fade" id="aboutModal" role="dialog" aria-labelledby="about-modal" aria-hidden="true">
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <h5 className="modal-title">About Approval</h5>
                                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div className="modal-body">
-                                {this.state.selectedUser.firstName}
-                                {this.state.selectedUser.lastName}
 
-                                <div className='img-size'>
-                                    <img alt='User photos' src={this.state.selectedUser.images[0]} />
+                                <div>
+                                    {/* name section */}
+
+                                    <h3 className='text-center text-info'>
+                                        {this.state.selectedUser.firstName} &nbsp; {this.state.selectedUser.lastName}
+                                    </h3>
+
+                                </div>
+
+                                <div>
+                                    <div>
+                                        <p className='p-5'>HAi friednd</p>
+                                        <div>
+                                            <button className="btn btn-danger pull-right ml-5 mr-3">Decline</button>
+                                            <button className="btn btn-info pull-right">Approve</button>
+                                        </div>
+                                    </div>
                                 </div>
 
                             </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Save changes</button>
-                            </div>
+
                         </div>
                     </div>
                 </div>
+
+
+                {/* selfie model html */}
+                <div className="modal fade" id="selfieModal" role="dialog" aria-labelledby="selfie-modal" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Selfie Approval</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+
+                                <div>
+
+                                    {/* name section */}
+                                    <h3 className='text-center text-info'>
+                                        {this.state.selectedUser.firstName} &nbsp; {this.state.selectedUser.lastName}
+                                    </h3>
+                                </div>
+
+                                <div>
+                                    <div>
+                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                        <div className="mt-3">
+                                            <button className="btn btn-danger pull-right ml-4 mr-5">Decline</button>
+                                            <button className="btn btn-info pull-right">Approve</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                {/* Photos model html */}
+                <div className="modal fade" id='photosModal' role="dialog" aria-labelledby="photos-modal" aria-hidden="true">
+                    <div className="modal-photos" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Photos Approval</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <div>
+                                    {/* name section */}
+                                    <h3 className='text-center text-info'>
+                                        {this.state.selectedUser.firstName} &nbsp; {this.state.selectedUser.lastName}
+                                    </h3>
+                                </div>
+
+                                <div>
+                                    <div>
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td>
+                                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                                        <button className="btn btn-danger ml-5 mt-3">Decline</button>
+                                                        <button className="btn btn-info ml-2 mt-3">Approve</button>
+                                                    </td>
+                                                    <td>
+                                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                                        <button className="btn btn-danger ml-5 mt-3">Decline</button>
+                                                        <button className="btn btn-info ml-2 mt-3">Approve</button>
+                                                    </td>
+                                                    <td>
+                                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                                        <button className="btn btn-danger ml-5 mt-3">Decline</button>
+                                                        <button className="btn btn-info ml-2 mt-3">Approve</button>
+                                                    </td>
+                                                    <td>
+                                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                                        <button className="btn btn-danger ml-5 mt-3">Decline</button>
+                                                        <button className="btn btn-info ml-2 mt-3">Approve</button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                                        <button className="btn btn-danger ml-5 mt-3">Decline</button>
+                                                        <button className="btn btn-info ml-2 mt-3">Approve</button>
+                                                    </td>
+                                                    <td>
+                                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                                        <button className="btn btn-danger ml-5 mt-3">Decline</button>
+                                                        <button className="btn btn-info ml-2 mt-3">Approve</button>
+                                                    </td>
+                                                    <td>
+                                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                                        <button className="btn btn-danger ml-5 mt-3">Decline</button>
+                                                        <button className="btn btn-info ml-2 mt-3">Approve</button>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                {/* ID model html */}
+                <div className="modal fade" id='idModal' role="dialog" aria-labelledby="id-modal" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Identification Approval</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+
+                                <div>
+
+                                    {/* name section */}
+                                    <h3 className='text-center text-info'>
+                                        {this.state.selectedUser.firstName} &nbsp; {this.state.selectedUser.lastName}
+                                    </h3>
+                                </div>
+
+                                <div>
+                                    <div>
+                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                        <img alt='User photos' className='img-size' src={this.state.selectedUser.images[0]} />
+                                        <div className="mt-3">
+                                            <button className="btn btn-danger pull-right ml-4 mr-5">Decline</button>
+                                            <button className="btn btn-info pull-right">Approve</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
             </div>
         );
     }
